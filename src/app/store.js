322f640ee797f037abe887184fca3cefe05b8966
JaRoD41 <laurent.trinco@gmail.com>
2023-11-07 09:49:11 +0100

    uninstall Redux

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import employee from '../features/employee';
-
-export default configureStore({
-  reducer: {
-    employee,
-  },
-});
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './style/index.css';
-import store from './app/store.js';
-import { Provider } from 'react-redux';
+// import store from './app/store.js';
+// import { Provider } from 'react-redux';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 );
