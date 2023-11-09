c9c3f4667a9602e8920b70f6b69af07a73dc983a
JaRoD41 <laurent.trinco@gmail.com>
2023-11-09 16:28:42 +0100

    employee list mock modif

--- src/utils/customHook.js
+++ src/utils/customHook.js
@@ -64,3 +64,63 @@ const EmployeeList = () => {
     </>
   );
 };
+
+// ----------------------------------------------------------------
+
+import { useContext } from 'react';
+import { EmployeeContext } from './EmployeeContext';
+
+export function useEmployees() {
+  const context = useContext(EmployeeContext);
+
+  if (!context) {
+    throw new Error('useEmployees must be used within an EmployeeProvider');
+  }
+
+  return context;
+}
+
+// Puis, dans le composant : 
+
+import React from 'react';
+import { useEmployees } from './useEmployees';
+
+export function EmployeeForm() {
+  const { addEmployee } = useEmployees();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newEmployee = {
+      /* vos données de formulaire ici */
+    };
+    addEmployee(newEmployee);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* vos champs de formulaire ici */}
+      <button type="submit">Save</button>
+    </form>
+  );
+}
+
+
+// Et dans le tableau d'employés :
+
+import React from 'react';
+import { useEmployees } from './useEmployees';
+
+export function EmployeeTable() {
+  const { employees } = useEmployees();
+
+  return (
+    <table>
+      {/* vos en-têtes de tableau ici */}
+      <tbody>
+        {employees.map((employee, index) => (
+          <tr key={index}>{/* vos données d'employé ici */}</tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
