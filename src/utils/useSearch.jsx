e941c93c8c7be3360f5e2551d6560180cdbe1f0e
JaRoD41 <laurent.trinco@gmail.com>
2023-11-14 16:01:23 +0100

    comment

--- src/utils/useSearch.jsx
+++ src/utils/useSearch.jsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
 import { EmployeeContext } from '../context/EmployeeContext';
 
+// I create a custom hook to make a global search in the table
 export function useSearch() {
   const { employeeList } = useContext(EmployeeContext);
-
   return { employeeList };
 }
