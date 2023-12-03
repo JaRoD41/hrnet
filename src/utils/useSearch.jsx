cfb01b0a021309cc5d1b627098d71feb11274fa7
JaRoD41 <laurent.trinco@gmail.com>
2023-12-03 14:50:10 +0100

    delete useless custom hook

--- src/utils/useSearch.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { EmployeeContext } from '../context/EmployeeContext';
-
-// I create a custom hook to make a global search in the table
-export function useSearch() {
-  const { employeeList } = useContext(EmployeeContext);
-  return { employeeList };
-}
